27bc0d836f64654498a64e04c15e79b604585079
PizzaKing29 <Legozap223@gmail.com>
2025-06-04 11:14:09 -0500

    initial commit

---
+++ main/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Proxy
+{
+    public static void Main()
+    {
+        IPAddress IpAddress = IPAddress.Loopback; // The default loopback IP is 127.0.0.1
+        TcpListener Listener = new TcpListener(IpAddress, 8888);
+
+        Listener.Start();
+
+        Console.WriteLine($"server is listening on IP/Port{Listener.LocalEndpoint}");
+    }
+}
